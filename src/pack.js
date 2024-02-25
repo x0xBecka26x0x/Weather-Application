1703f6d6bc2de7f6609f2b0329a7fbd8d425f32d
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-02-25 18:53:42 -0500

    added JS file & the rest of the CSS

---
+++ src/pack.js
@@ -0,0 +1,10 @@
+function searchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#box-search");
+    
+}
+
+
+
+let boxSearchElement = document.querySelector("#box-search");
+boxSearchElement.addEventListener("submit", searchSubmit);
