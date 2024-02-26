596b005565f538f1da93431d728a811cc7510ab7
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-02-25 19:10:22 -0500

    update JS file

--- src/pack.js
+++ src/pack.js
@@ -1,10 +1,11 @@
 function searchSubmit(event) {
     event.preventDefault();
-    let searchInput = document.querySelector("#box-search");
-    
-}
-
-
-
-let boxSearchElement = document.querySelector("#box-search");
-boxSearchElement.addEventListener("submit", searchSubmit);
+    let searchInput = document.querySelector("#search-form");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+  }
+  
+  let boxSearchElement = document.querySelector("#box-search");
+  boxSearchElement.addEventListener("submit", searchSubmit);
+  
+  
