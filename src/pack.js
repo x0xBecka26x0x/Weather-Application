9c6138c1c80071921264b2791041842fa36fcfed
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-02-28 16:39:02 -0500

    update axios

--- src/pack.js
+++ src/pack.js
@@ -1,11 +1,17 @@
+function searchCity(city) {
+    let apiKey = "bf8f1010b3c486eaa378at4e5eo24f84";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial';
+    console.log(apiUrl);
 
+}
 
 function search(event) {
     event.preventDefault();
-    let searchInputElement = document.querySelector("#search-input");
+    let searchInput = document.querySelector("#search-input");
     let cityElement = document.querySelector("#current-city");
-    cityElement.innerHTML = searchInputElement.value;
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
     
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+let searchInputElement = document.querySelector("#search-input");
+searchInputElement.addEventListener("submit", search);
