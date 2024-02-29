23a14dac24bd9caeda39b6e64460b4039605c637
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-02-29 17:15:51 -0500

    updating weather part of function

--- src/pack.js
+++ src/pack.js
@@ -1,13 +1,22 @@
+function weatherInfo(response) {
+    let temperatureElement = document.querySelector("temperature");
+    let temperature = response.data.temperature.current;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
 
 function searchCity(city) {
     let apiKey = "bf8f1010b3c486eaa378at4e5eo24f84";
-    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial';
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(weatherInfo); 
 }
 
 function search(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = searchInput.value;
     
     searchCity(searchInput.value);
 }
@@ -15,5 +24,4 @@ function search(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
 
-searchCity("Paris");
 
