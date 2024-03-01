d14bce911e3a4f3479548835f5b9e7f00a29ec75
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-03-01 17:42:11 -0500

    entered more weather data

--- src/pack.js
+++ src/pack.js
@@ -2,9 +2,39 @@ function weatherInfo(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#condition");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
     cityElement.innerHTML = response.data.city;
-   temperatureElement.innerHTML = Math.round(temperature);
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}m/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday",
+     "Monday", 
+     "Tuesday", 
+     "Wednesday", 
+     "Thursday", 
+     "Friday", 
+     "Saturday",
+    ];
+
+    let day = days [date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
